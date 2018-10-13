7559d4f2fd2faa1c20e2e3d3a1ef047ce27509df
Adrien Horgnies <adrien.pierre.horgnies@gmail.com>
2018-10-13 15:53:32 +0200

    head start to create database schema
    - expose mysql port to be able to work with intelliJ MySQL plugin
    - configure database to use utf8mb4 encoding and collation
    - create a first table to show an example
    
    closes #15

---
+++ src/main/docker/mysql/script.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `coworking`.`user` ( 
+    `id` SERIAL NOT NULL AUTO_INCREMENT COMMENT 'identify a user unequivocally',
+    `email` VARCHAR(255) NOT NULL COMMENT 'contact user, send private information or reset password',
+    `password` CHAR(60) BINARY NOT NULL COMMENT 'cryptographic hash of user password',
+    `lastname` VARCHAR(100) NOT NULL COMMENT 'family name used to address user in communication',
+    `firstname` VARCHAR(100) NOT NULL COMMENT 'first name used to address user in communication',
+    PRIMARY KEY (`id`),
+    UNIQUE (`email`)) ENGINE = InnoDB;
+
+INSERT INTO `coworking`.`user` (`id`, `email`, `password`, `lastname`, `firstname`) VALUES
+    (NULL, 'adrien.pierre.horgnies@gmail.com', '$2y$10$rbE59kdmEfSgKQL8cvRBHuRzKktjVXRpICafsx5d7/SITS4zEcInS', 'Horgnies', 'Adrien'),
+    (NULL, 'baran.1702.student@ifosupwavre.be', '$2y$10$7kohD8s7JhspL5goSZ1JT.f5jcVXvOk4Vjt.6GATWQi/lnsJuoklK', 'Baran', 'Katia'),
+    (NULL, 'chan.1804.student@ifosupwavre.be', '$2y$10$78lnxSlBaWpyTd7Z/W7tu.NaaQN4fgZUBSOJM5THmKFtVc/l5VyLC', 'Chan', 'Phirum'),
+    (NULL, 'thiry.2906.student@ifosupwavre.be', '$2y$10$ny6EgjXFr8ZGHf9IQZ7L0uLLoNQPh9Ilbeih3fgSjORNis6QOq4BW', 'Thiry', 'Stéphane'),
+    (NULL, 'stephane.huguier@ifosupwavre.be', '$2y$10$F6G9d2X8jWvuTNYZb1Jme.rSAK4fLkliOhMJsAoarGPv/uHoUJ43q', 'Huguier', 'Stéphane'),
+    (NULL, 'xavier.jeunejean@ifosupwavre.be', '$2y$10$ruYha1JkYyMkFimirQiuO.qEFeVDnIR7fhek7PFhCM2I/tvL5P1Ky', 'Jeunejean', 'Xavier')
