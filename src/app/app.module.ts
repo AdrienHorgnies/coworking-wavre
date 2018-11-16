f5aea7aaba3841b0fff3cbcd4be33b7823e89bec
TCO <katia@bbcom.be>
2018-11-16 15:57:26 +0100

    tentative to fix css bug and a few other things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {AppComponent} from './app.component';
 import {SearchComponent} from './search/search.component';
+import {MeetingsComponent} from './meetings/meetings.component';
+import {MeetingComponent} from './meeting/meeting.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
+      MeetingsComponent,
+      MeetingComponent
   ],
   imports: [
     BrowserModule
