5a1ee8b4cb7d6249e6f2238483fdc1c9453668ba
Adrien Horgnies <aHorgnies@altissia.com>
2018-11-17 15:35:27 +0100

    delete unrequested components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { RouterModule } from "@angular/router";
-import { FormsModule} from "@angular/forms";
 
 import { ROUTES } from "./app.routes";
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { MenuComponent } from './menu/menu.component';
-import { SearchComponent } from './search/search.component';
-import { UsersComponent } from './users/users.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    MenuComponent,
-    SearchComponent,
-    UsersComponent,
+      RegisterComponent
 ],
   imports: [
     BrowserModule,
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { Observable} from "rxjs/index";
-import { UserModel} from "./Models/user.model";
+import { Observable } from "rxjs";
+import { UserModel } from "./models/user.model";
 
-@Injectable ({
+@Injectable({
     providedIn: 'root'
 })
 
 export class UserService {
 
-constructor (private http: HttpClient  ){}
+    constructor(private http: HttpClient) {
+    }
 
-register(firstname: string, lastname: string, email: string, password: string): Observable<any> {
-        const body = { firstname, lastname, email, password };
+    register(firstName: string, lastName: string, email: string, password: string): Observable<any> {
+        const body = {firstName, lastName, email, password};
         return this.http.post<UserModel>('http://localhost:8080/api/users', body);
     }
 
