598fba105a9103a1570557563d0f7902419d4415
TCO <katia@bbcom.be>
2018-11-22 16:18:19 +0100

    Réservation Meeting - debut

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {SpacesComponent} from './spaces/spaces.component';
 import { SpaceComponent } from './space/space.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { BookingmeetingComponent } from './bookingmeeting/bookingmeeting.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         SpacesComponent,
-        SpaceComponent
+        SpaceComponent,
+        TabsComponent,
+        BookingmeetingComponent
     ],
     imports: [
         BrowserModule
