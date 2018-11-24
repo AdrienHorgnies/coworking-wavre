4f58193388c597a537cae4e74866fed6cfe6415c
TCO <katia@bbcom.be>
2018-11-24 23:40:42 +0100

    booking spaces private, open, bubble

--- src/app/bookingspaceprivate/bookingspace.component.ts
+++ src/app/bookingspaceprivate/bookingspace.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {toDate} from "@angular/common/src/i18n/format_date";
+import {formatDate} from "@angular/common";
 
 @Component({
   selector: 'cow-bookingspace',
@@ -8,11 +10,18 @@ import { Component, OnInit } from '@angular/core';
 export class BookingspaceComponent implements OnInit {
 
     content = "";
+    date1: string = "25/02/2019";
+
+    date2 = "";
+    // totaldate = {{date1}};
 
     // todo: initialiser le nombre de personnes >1 et <capacity
     numberpers: string = "number";
 
+    total(montant: string): string {
+        return montant;
 
+}
     constructor() { }
 
   ngOnInit() {
