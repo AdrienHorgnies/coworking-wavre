8f09a82ceab9f4186ad763494b439ab149967e66
Chan Phirum <33428960+Phimeria@users.noreply.github.com>
2018-11-27 17:31:38 +0100

    amélioratio formulaire - nouvelle branche

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { SpaceService } from "../space.service";
 import { SpaceModel } from "../models/space.model";
+import { ImageService } from "../image.service";
+
 
 @Component({
     selector: 'cow-reservation-form',
@@ -10,10 +14,33 @@ export class ReservationFormComponent implements OnInit {
 
     @Input() space: SpaceModel;
 
-    constructor() {
+    content = '';
+    date1: string = '25/02/2019';
+    date2 = '';
+    display: boolean = true;
+
+    // todo: initialiser le nombre de personnes >1 et <capacity
+    numberpers: string = 'number';
+
+    total(montant: string): string {
+        return montant;
+    }
+
+
+    spaces: Array<SpaceModel>;
+    spacesSubscription: Subscription;
+
+    constructor(private spaceService: SpaceService, public imageService: ImageService) {
     }
 
     ngOnInit() {
+        this.spacesSubscription = this.spaceService.list().subscribe(spaces => this.spaces = spaces);
     }
 
+    ngOnDestroy() {
+        if (this.spacesSubscription) {
+            this.spacesSubscription.unsubscribe();
+        }
+    }
 }
+
