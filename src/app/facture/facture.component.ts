b43c430fae9e0f7635a1ddded26053db4710627c
Chan Phirum <33428960+Phimeria@users.noreply.github.com>
2018-11-29 16:15:57 +0100

    add calendar and bill

---
+++ src/app/facture/facture.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cow-facture',
+  templateUrl: './facture.component.html',
+  styleUrls: ['./facture.component.css']
+})
+export class FactureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
