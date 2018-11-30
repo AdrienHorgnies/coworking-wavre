9ef6243d16aa093f80d4b46e1b10203b742027f4
Adrien Horgnies <aHorgnies@altissia.com>
2018-11-30 13:01:37 +0100

    remove unused variable colors

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,21 +1,6 @@
 import { Component, TemplateRef, ViewChild } from '@angular/core';
 import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
-const colors: any = {
-    red: {
-        primary: '#ad2121',
-        secondary: '#FAE3E3'
-    },
-    blue: {
-        primary: '#1e90ff',
-        secondary: '#D1E8FF'
-    },
-    yellow: {
-        primary: '#e3bc08',
-        secondary: '#FDF1BA'
-    }
-};
-
 @Component({
     selector: 'cow-profile',
     templateUrl: './profile.component.html',
@@ -26,8 +11,6 @@ export class ProfileComponent {
 @ViewChild('modalContent')
     modalContent: TemplateRef<any>;
 
-
     constructor(private modal: NgbModal) {}
-
 
 }
