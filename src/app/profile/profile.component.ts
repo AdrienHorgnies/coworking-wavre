ad6d884debea849a2bcc63b4209dde13afa5c0d7
Adrien Horgnies <aHorgnies@altissia.com>
2018-11-30 13:15:20 +0100

    make profile retrieve real user information

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,8 @@
-import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { UserService } from "../user.service";
+import { UserModel } from "../models/user.model";
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'cow-profile',
@@ -7,10 +10,25 @@ import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
     styleUrls: ['./profile.component.css']
 })
 
-export class ProfileComponent {
-@ViewChild('modalContent')
+export class ProfileComponent implements OnInit, OnDestroy {
+    @ViewChild('modalContent')
     modalContent: TemplateRef<any>;
 
-    constructor(private modal: NgbModal) {}
+    user: UserModel;
+    userSubscription: Subscription;
 
+    constructor(private userService: UserService, private modal: NgbModal) {
+    }
+
+    ngOnInit() {
+        this.userSubscription = this.userService.getCurrentlyLoggedInUser().subscribe(
+            user => this.user = user
+        );
+    }
+
+    ngOnDestroy() {
+        if (this.userSubscription) {
+            this.userSubscription.unsubscribe();
+        }
+    }
 }
